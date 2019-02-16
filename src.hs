633388a486169341df6bcb899b59dadfcf86a7e8
bfazeli <bfazeli1@gmail.com>
2019-02-16 11:59:30 -0800

    Update: Implemented a bottom up recursion of fib

--- src.hs
+++ src.hs
@@ -1,12 +1,30 @@
+-- while loop
+while:: state -> (state -> Bool) -> (state -> state) -> (state -> result) -> result
+while state shouldContinue action finalAction =
+    if shouldContinue state then while (action state) shouldContinue action finalAction
+    else finalAction state
+--
+
 -- fibTopDownRec
 fibTopDownRec :: Int -> Int
 fibTopDownRec 1 = 1
 fibTopDownRec 2 = 1
-fibTopDownRec n = fibTopDownRec (n - 1) + fibTopDownRec(n - 2)
+fibTopDownRec n = 
+    fibTopDownRec (n - 1) + fibTopDownRec(n - 2)
 --
+
+-- fibBottomUpIter
+fibBottomUpIter:: Int -> Int
+fibBottomUpIter n =
+    while 
+        (1, 1, 0) 
+        (\(c, _, _) -> c < n) 
+        (\(c, ka, kb) -> (c + 1, ka + kb, ka))
+        (\(c, ka, kb) -> ka)
 
 -- main execution of program
 main :: IO()
 main = 
-    print(fibTopDownRec 6)
+    print(fibTopDownRec 6) >>
+    print(fibBottomUpIter 6)
 --
